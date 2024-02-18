297b31f53ce0a720b7215f279849be8f45d43ccd
KrishnaM023 <krishna050120>
2024-02-18 15:38:23 +0530

    One more controller and router is add for posts

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,10 @@ const homeController = require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/', homeController.home); // home is exported from home_Controlle file
+router.use('/users', require('./users'));
+router.use('/posts', require('./posts'));
+
+// For any further routes, access from here
+// router.use('/routerName', require('./routerFile'));
 
 module.exports = router;

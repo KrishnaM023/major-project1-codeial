a018ecb82c0713ab09c9edd9fd818e8704508ca9
KrishnaM023 <krishna050120>
2024-02-21 09:44:00 +0530

    config models and and a sign up page created

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,84 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.render('user_profile', {
         title: "User Profile"
     })
 }
+
+// Render the Sign Up Page
+module.exports.signUp = function(req, res) {
+    return res.render('user_sign_up', {
+        title: "Codieal | Sign Up"
+    })
+}
+
+
+// Render the Sign In Page
+module.exports.signIn = function(req, res) {
+    return res.render('user_sign_in', {
+        title: "Codieal | Sign In"
+    })
+}
+
+// Render the Sign In Page
+
+module.exports.create = function(req, res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}).then(function(user){
+        if(user){
+            return res.redirect('back'); // User already exists, redirect back
+        } else {
+            // User does not exist, create a new user
+            User.create(req.body).then(function(user){
+                return res.redirect('/users/sign-in'); // Redirect to sign-in page after successful creation
+            }).catch(function(err){
+                console.log('error in creating user while signing up', err);
+                return res.redirect('back'); // Redirect back if there's an error
+            });
+        }
+    }).catch(function(err){
+        console.log('error in finding user in signing up', err);
+        return res.redirect('back'); // Redirect back if there's an error
+    });
+}
+
+
+
+
+// Sign In and Create a session for the User
+module.exports.createSession = function(req, res){
+    // TODO later
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// Sign In and Create a session for the User
+module.exports.createSession = function(req, res){
+    // TODO later
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 
+app.use(express.urlencoded());
+
+// Setting up Cookie
+app.use(cookieParser());
+
 // Layouts
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+app.use(express.static('./assets'));
 
 app.use(expressLayouts);
 
@@ -11,7 +20,6 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-app.use(express.static('./assets'));
 
 // Use express router
 app.use('/', require('./routes'));
